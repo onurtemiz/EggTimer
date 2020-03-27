741eeacac7c9e18076b795cf3328d78ed302061b
Onur Temiz <onurmain@gmail.com>
2020-03-27 10:04:56 +0300

    Egg Time

---
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.eggtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView timerView;
+    private Button goButton;
+    private SeekBar seekBar;
+    private CountDownTimer countDownTimer;
+    private int maxSec = 1500;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerView = (TextView) findViewById(R.id.timerView);
+        goButton = (Button) findViewById(R.id.goButton);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), getResources().getIdentifier("airhorn","raw",getPackageName()));
+        seekBar.setMax(maxSec);
+        seekBar.setProgress(maxSec/2);
+        timerView.setText(getStringTime(seekBar.getProgress()));
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                timerView.setText(getStringTime(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                countDownTimer.cancel();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+
+
+        });
+
+        changeCountTimer(seekBar.getProgress());
+    }
+
+
+    public String getStringTime(int time){
+        String strMinutes = String.valueOf(time/60).length() == 1 ? "0" + (time/60) : String.valueOf(time/60);
+        String strSeconds = String.valueOf(time%60).length() == 1 ? "0" + (time%60) : String.valueOf(time%60);
+
+        return (strMinutes + ":" + strSeconds);
+    }
+
+
+    public void startTimer(View view){
+        this.countDownTimer.cancel();
+        changeCountTimer(seekBar.getProgress());
+        this.countDownTimer.start();
+
+    }
+
+
+    public void changeCountTimer(int sec){
+        this.countDownTimer = new CountDownTimer(sec*1000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                seekBar.setProgress(seekBar.getProgress()-1);
+
+            }
+
+            @Override
+            public void onFinish() {
+                mediaPlayer.start();
+            }
+        };
+    }
+}
